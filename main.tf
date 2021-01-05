bd9386e5a1c535bde73a58f7514ce2fcc9ed66bc
Sayali Joshi <sayali.joshi@magicmemories.com>
2021-01-05 04:25:55 +0000

    testing

--- main.tf
+++ main.tf
@@ -1,4 +1,15 @@
 # Terraform configuration
+# Terraform configuration
+terraform {
+  backend "remote" {
+    hostname      = "app.terraform.io"
+    organization  = "MagicMemories"
+
+    workspaces {
+      prefix = "fun"
+    }
+  }
+}
 
 variable "name_length" {
   description = "The number of words in the pet name"
